07fa66ce0aaa363ce4e0be1cafff40dfb98ecb5d
Jumi <feliciastrandberg@gmail.com>
2024-01-10 15:09:59 +0100

    added calc. btn and divs for input and output

---
+++ ts/main.ts
@@ -0,0 +1,12 @@
+// Annuity fomula 
+
+function annunityFormula(principal: number, annualInterest:number, numberOfPayments:number): number {
+    // Divided by months in a year and 100 to make it decimal 
+    const monthlyInterest = annualInterest / 12 / 100; 
+    // Part of the given formula that calculate present value of future payments 
+    const discountFactor = ((1 + monthlyInterest) ** numberOfPayments -1) / 
+    (monthlyInterest * (1 + monthlyInterest) ** numberOfPayments); 
+
+    const monthlyPayment = principal / discountFactor; 
+    return monthlyPayment; 
+}
