b257ac96912d9cf38830a067c2f0da65b7749b38
Jumi <feliciastrandberg@gmail.com>
2024-01-10 15:27:49 +0100

    added eventListeners to check for input on input fields

--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,9 @@
-// Annuity fomula 
+// Get HTML elements
+const principal = document.getElementById('loan') as HTMLInputElement;
+const interest = document.getElementById('interest') as HTMLInputElement;
+const time = document.getElementById('time') as HTMLInputElement;
 
+// Annuity fomula 
 function annunityFormula(principal: number, annualInterest:number, numberOfPayments:number): number {
     // Divided by months in a year and 100 to make it decimal 
     const monthlyInterest = annualInterest / 12 / 100; 
@@ -12,5 +16,17 @@ function annunityFormula(principal: number, annualInterest:number, numberOfPayme
 }
 
 // Get value of loan amount (principal) 
+principal.addEventListener('input', (e) => {
+    const principalValue = parseFloat(principal.value);
+})
+
 // Get value of interest rate (in percentage)
+interest.addEventListener('input', (e) => {
+    const interestValue = parseFloat(interest.value);
+})
+
 // Get value of loan period (in years)
+time.addEventListener('input', (e) => {
+    const timeValue = parseFloat(time.value); 
+})
+
